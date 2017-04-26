57208d2e4160909313a9150d5865fe721cce80cf
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-04-26 14:48:58 -0800

    Modify the livingspace.py file

--- app/livingspace.py
+++ app/livingspace.py
@@ -7,3 +7,20 @@ class Livingspace(Room):
         capacity = 4
         room_type = 'Livingspace'
         livingspaces_list = []
+        self.livingspace_occupants_list = []
+
+    def check_office_assignable(self):
+        if len(self.livingspace_occupants_list) < self.capacity:
+            return True
+        else:
+            return 'Office is full'
+
+    def assign_allocated_office(self, name):
+        if len(self.livingspace_occupants_list) < self.capacity:
+            self.livingspace_occupants_list.append(name)
+            return True
+        else:
+            return False
+
+    def length_of_office_list(self):
+        return len(self.livingspace_occupants_list)
