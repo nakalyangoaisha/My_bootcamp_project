b3c6b93a8a9a88027a3b3b45fb560e6c4cd5efc4
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-04-26 14:48:22 -0800

    Modify the office.py file

--- app/office.py
+++ app/office.py
@@ -6,3 +6,31 @@ class Office(Room):
         capacity = 6
         room_type = 'office'
         Room.__init__(self, room_name, capacity, room_type)
+        self.office_occupants_list = []
+
+    def check_office_assignable(self):
+        if len(self.office_occupants_list) < self.capacity:
+            return True
+        else:
+            return 'Office is full'
+
+    def assign_allocated_office(self, name):
+        if len(self.office_occupants_list) < self.capacity:
+            self.office_occupants_list.append(name)
+            return True
+        else:
+            return False
+
+    def length_of_office_list(self):
+        return len(self.office_occupants_list)
+
+
+        # Create a list that holds the people allocated to this office
+
+        # Create a function that allows you to add allocated people to the list
+
+        # Check if you can still allocate more people
+
+        # Return number of people in the list
+
+        # Return whether the room is still allocatable
