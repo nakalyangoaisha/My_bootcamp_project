ce87d35160dcab7c900786a477c61076b89b3b9e
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-04-26 11:47:45 -0800

    Modify the office.py file

--- app/office.py
+++ app/office.py
@@ -4,5 +4,5 @@ from app.room import Room
 class Office(Room):
     def __init__(self, room_name):
         capacity = 6
-        room_type = 'Office'
+        room_type = 'office'
         Room.__init__(self, room_name, capacity, room_type)
