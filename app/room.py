40d2fd495be1266c2616b2ccc17b2fe4873ebd19
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-04-26 11:53:31 -0800

    Modify the livingspace.py file

--- app/room.py
+++ app/room.py
@@ -1,5 +1,6 @@
-class Room():
+class Room:
     def __init__(self, room_name, capacity, room_type):
         self.room_name = room_name
         self.capacity = capacity
         self.room_type = room_type
+
