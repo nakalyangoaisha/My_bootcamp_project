49d1eac41cd1ec1f76de24afffae2bfe347379aa
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-04-27 15:58:27 -0800

    Make changes to fellow.py

--- app/fellow.py
+++ app/fellow.py
@@ -2,7 +2,5 @@ from app.person import Person
 
 
 class Fellow(Person):
-    def __init__(self, name, position):
-        super().__init__(name, position)
-        self.person_name = name
-        position = 'Fellow'
+
+    position = 'fellow'
