3f7612502d193718939f3690ba473fc48423940c
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-04-27 15:53:08 -0800

    Modify file livingspaces.py

--- app/livingspace.py
+++ app/livingspace.py
@@ -2,25 +2,5 @@ from app.room import Room
 
 
 class Livingspace(Room):
-    def __init__(self, room_name, capacity, room_type):
-        super().__init__(room_name, capacity, room_type)
-        capacity = 4
-        room_type = 'Livingspace'
-        livingspaces_list = []
-        self.livingspace_occupants_list = []
-
-    def is_livingspace_assignable(self):
-        if len(self.livingspace_occupants_list) < self.capacity:
-            return True
-        else:
-            return False
-
-    def assign_allocated_staff_livingspace(self, name):
-        if len(self.livingspace_occupants_list) < self.capacity:
-            self.livingspace_occupants_list.append(name)
-            return True
-        else:
-            return False
-
-    def len_livingspace_occupants_list(self):
-        return len(self.livingspace_occupants_list)
+    capacity = 4
+    room_type = 'livingspace'
