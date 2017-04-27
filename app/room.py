751c3043d2153dcf8bad0e5eb1d820474daa463f
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-04-27 15:54:50 -0800

    update to file room.py

--- app/room.py
+++ app/room.py
@@ -1,6 +1,29 @@
-class Room:
-    def __init__(self, room_name, capacity, room_type):
-        self.room_name = room_name
-        self.capacity = capacity
-        self.room_type = room_type
+from abc import ABCMeta
 
+
+class Room(object):
+    __metaclass__ = ABCMeta
+
+    capacity = 0
+    room_type = ""
+
+    def __init__(self, room_name, room_type):
+        self.room_name = room_name
+        self.room_type = room_type
+        self.room_occupants_list = []
+
+    def is_room_assignable(self, room_name):
+        if len(room_name.self.room_occupants_list) < self.capacity:
+            return True
+        else:
+            return False
+
+    def len_room_occupants_list(self):
+        return len(self.room_occupants_list)
+
+    def add_person_to_room(self, person):
+        if self.is_room_assignable(self.room_name):
+            self.room_occupants_list.append(person)
+            return True
+        else:
+            return False
