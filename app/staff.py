c615a9f1518b0a9e1c0a32b1058817460aaf9a59
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-04-27 15:55:50 -0800

    Make changes to staff.py

--- app/staff.py
+++ app/staff.py
@@ -2,7 +2,4 @@ from app.person import Person
 
 
 class Staff(Person):
-    def __init__(self, name, position):
-        super().__init__(name, position)
-        self.name = name
-        position = 'staff'
+    position = 'staff'
