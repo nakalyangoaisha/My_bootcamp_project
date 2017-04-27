325862d2cb337c46841d59cee2851d812763060b
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-04-27 15:54:03 -0800

    Make changes to file tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,6 @@
 import unittest
-from random import Random
-from unittest import TestCase
 
-from app.dojo import Dojo, Dojo_instance
+from app.dojo import Dojo
 from app.room import Room
 from app.person import Person
 from app.livingspace import Livingspace
@@ -11,72 +9,125 @@ from app.fellow import Fellow
 from app.staff import Staff
 
 
-class Test_ClassRelationships(TestCase):
-    def test_office_is_subclass_of_Room(self):
-        self.assertTrue(issubclass(Office, Room), msg='Office is not a subclass of Room')
+class Test_Dojo(unittest.TestCase):
+    def setUp(self):
+        self.dojo_instance = Dojo()
 
-    def test_livingspace_is_subclass_of_Room(self):
-        self.assertTrue(issubclass(Livingspace, Room), msg='Livingspace is not a subclass of Room')
+    # tests for the create_room_method
 
-    def test_fellow_is_subclass_of_Person(self):
-        self.assertTrue(issubclass(Fellow, Person), msg=' Fellow is not a subclass of Person')
+    def test_returns_error_when_room_name_and_type_are_non_strings(self):
+        self.assertEqual(self.dojo_instance.create_room(
+            0, 2), 'room_name and room_type should be strings',
+            msg='Room name and type must only be strings.')
 
+    def test_create_room_successfully(self):
+        # checks if length of the rooms_dict and available rooms was incremented when room is added
+        initial_room_count = len(self.dojo_instance.rooms_dict)
+        initial_available_rooms_count = len(self.dojo_instance.offices['available'])
+        aisha = self.dojo_instance.create_room('a', 'office')
+        self.assertTrue(aisha, msg='Object does not exist')
+        new_room_count = len(self.dojo_instance.rooms_dict)
+        new_available_rooms_count = len(self.dojo_instance.offices['available'])
+        self.assertEqual(new_room_count - initial_room_count, 1)
+        self.assertEqual(new_available_rooms_count - initial_available_rooms_count, 1)
+
+        # checks that room is only created once
+        aisha = self.dojo_instance.create_room('a', 'office')
+        self.assertTrue('Room already exists.')
+
+    # tests for the add_person method
+
+    def test_returns_error_when_name_and_position_are_non_strings(self):
+        self.assertEqual(self.dojo_instance.add_person(
+            0, 2), 'Name and position should be strings',
+            msg='Name and position must only be strings.')
+
+    def test_returns_error_when_name_and_position_are_empty_strings(self):
+        self.assertEqual(self.dojo_instance.add_person('', ''), 'Name and position can not be empty strings',
+                         msg='Name and position must not be empty')
+
+    def test_add_person_successfully(self):
+        self.dojo_instance = Dojo()
+
+    def tests_if_length_of_people_dict_increased_when_person_is_added_and_is_only_added_once(self):
+        # this also tests that person is added to the people_dict
+        initial_people_count = len(self.dojo_instance.people_dict)
+        k = self.dojo_instance.add_person('kim', 'staff')
+        self.assertTrue(k)
+        new_people_count = len(self.dojo_instance.people_dict)
+        self.assertEqual(new_people_count - initial_people_count, 1)
+
+        k = self.dojo_instance.add_person('kim', 'staff')
+        self.assertTrue('Person already exists.')
+
+    def test_that_staff_are_not_accommodated(self):
+        m = self.dojo_instance.add_person('kim', 'staff', 'Yes')
+        self.assertEqual(m, 'Staff cannot be accommodated')
+
+    # tests for the allocate room method
+    def test_that_both_name_and_room_type_are_strings(self):
+        self.assertEqual(self.dojo_instance.add_person(
+            0, 2), 'Name and room_type should be strings')
+
+    def tests_that_both_name_room_type_are_not_empty_strings(self):
+        self.assertEqual(self.dojo_instance.add_person('', ''), 'name and room_type can not be empty')
+
+    def test_that_both_staff_and_fellows_are_allocated_offices(self):
+
+
+
+
+
+
+
+
+class Test_class_Person(unittest.TestCase):
+    def test_person_is_instance_of_class_Person(self):
+        person = Person('name', 'position')
+        self.assertTrue(isinstance(person, Person), msg='person is not an instance of the Person class')
+
+
+class Test_class_Staff(unittest.TestCase):
     def test_staff_is_subclass_of_Person(self):
         self.assertTrue(issubclass(Staff, Person), msg='Staff is not a subclass of Person')
 
-
-class TestClasses(unittest.TestCase):
-    def setUp(self):
-        self.Dojo_instance = Dojo()
-        global random
-
-    def test_office(self):
-        office = Office('room_name')
-        self.assertTrue(isinstance(office, Office), msg='office is not an instance of the Office class')
-
-    def test_livingspace(self):
-        livingspace = Livingspace('room_name', 'capacity', 'room_type')
-        self.assertTrue(isinstance(livingspace, Livingspace),
-                        msg='livingspace is not an instance of the Living space class')
-
-    def test_staff(self):
+    def test_staff_is_instance_of_class_Staff(self):
         staff = Staff('name', 'position')
         self.assertTrue(isinstance(staff, Staff), msg='staff is not an instance of the Staff class')
 
-    def test_fellow(self):
+
+class Test_class_Fellow(unittest.TestCase):
+    def test_fellow_is_subclass_of_Person(self):
+        self.assertTrue(issubclass(Fellow, Person), msg=' Fellow is not a subclass of Person')
+
+    def test_fellow_isinstance_of_class_Fellow(self):
         fellow = Fellow('name', 'position')
-        self.assertTrue(isinstance(fellow, Fellow), msg='fellow is not an instance of the Fellow')
-
-    def test_create_room_successfully(self,):
-        initial_room_count = len(self.Dojo_instance.room_list)
-        a_office = Dojo_instance.create_room('a', 'office')
-        self.assertTrue(a_office)
-        new_room_count = len(Dojo_instance.room_list)
-        self.assertEqual(new_room_count - initial_room_count, 1)
-
-        b_livingspace = Dojo_instance.create_room('b', 'livingspace')
-        self.assertTrue(b_livingspace)
-        new_room_count = len(Dojo_instance.room_list)
-        self.assertEqual(new_room_count - initial_room_count, 2)
-
-    def test_add_person_successfully(self):
-        initial_people_count = len(Dojo_instance.people_list)
-        aisha_fellow = Dojo_instance.add_person('aisha', 'fellow')
-        self.assertTrue(aisha_fellow)
-        new_people_count = len(Dojo_instance.people_list)
-        self.assertEqual(new_people_count - initial_people_count, 1)
-
-        noor_staff = Dojo_instance.add_person('noor', 'staff')
-        self.assertTrue(noor_staff)
-        new_people_count = len(Dojo_instance.people_list)
-        self.assertEqual(new_people_count - initial_people_count, 2)
+        self.assertTrue(isinstance(fellow, Fellow), msg='fellow is not an instance of the Fellow class')
 
 
+class Test_class_Room(unittest.TestCase):
+    def test_if_room_isinstance_of_class_Room(self):
+        room = Room('room_name', 'capacity')
+        self.assertTrue(isinstance(room, Room), msg='room is not an instance of the Room class')
 
 
+class Test_class_Office(unittest.TestCase):
+    def test_office_is_subclass_of_Room(self):
+        self.assertTrue(issubclass(Office, Room), msg='Office is not a subclass of Room')
+
+    def test_office(self):
+        office = Office('room_name', 'room_type')
+        self.assertTrue(isinstance(office, Office), msg='office is not an instance of the Office class')
 
 
+class Test_class_Livingspace(unittest.TestCase):
+    def test_livingspace_is_subclass_of_Room(self):
+        self.assertTrue(issubclass(Livingspace, Room), msg='Livingspace is not a subclass of Room')
 
+    def test_livingspace(self):
+        livingspace = Livingspace('room_name', 'capacity', )
+        self.assertTrue(isinstance(livingspace, Livingspace),
+                        msg='livingspace is not an instance of the Living space class')
 
 
 if __name__ == '__main__':
