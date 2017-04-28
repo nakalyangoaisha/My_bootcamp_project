962dc0b2b3411dced1a35a33521b1f1c521a36a3
nakalyangoaisha <aishanakalyango1@gmail.com>
2017-04-28 10:08:24 -0800

    Update file room.py

--- app/room.py
+++ app/room.py
@@ -12,8 +12,8 @@ class Room(object):
         self.room_type = room_type
         self.room_occupants_list = []
 
-    def is_room_assignable(self, room_name):
-        if len(room_name.self.room_occupants_list) < self.capacity:
+    def is_room_assignable(self):
+        if len(self.room_occupants_list) < self.capacity:
             return True
         else:
             return False
@@ -22,7 +22,7 @@ class Room(object):
         return len(self.room_occupants_list)
 
     def add_person_to_room(self, person):
-        if self.is_room_assignable(self.room_name):
+        if self.is_room_assignable():
             self.room_occupants_list.append(person)
             return True
         else:
